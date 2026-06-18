c0094e1e132cae397e7a2114cf52b0f1ad91f18c
manthri-kumar <153302220+manthri-kumar@users.noreply.github.com>
2026-06-18 14:54:50 +0530

    dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -135,7 +135,7 @@ const Dashboard = () => {
         {/* Dynamic Typography Greeting Hub */}
         <div className="greeting">
           <h2>
-            {t("hello")} {userName}
+            {t("hello,")} {userName}
           </h2>
           <p>
             {city ? (
